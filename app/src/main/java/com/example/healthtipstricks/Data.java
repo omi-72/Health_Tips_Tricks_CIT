fcabde2a233b5f673a6623db244ea1d29e93fe07
omi-72 <jannat.omi722@gmail.com>
2022-05-20 22:37:29 +0600

    Create data class & generate contructor, ghetter & shetter also

---
+++ app/src/main/java/com/example/healthtipstricks/Data.java
@@ -0,0 +1,37 @@
+package com.example.healthtipstricks;
+
+public class Data {
+
+    int img;
+    String title, desc;
+
+    public Data(int img, String title, String desc) {
+        this.img = img;
+        this.title = title;
+        this.desc = desc;
+    }
+
+    public int getImg() {
+        return img;
+    }
+
+    public void setImg(int img) {
+        this.img = img;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+}
