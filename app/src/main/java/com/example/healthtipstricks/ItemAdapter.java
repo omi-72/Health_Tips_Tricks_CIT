f667946b7ccc57a6ad72d0f065c80be06051e191
omi-72 <jannat.omi722@gmail.com>
2022-05-20 23:53:09 +0600

    Generate constructor

--- app/src/main/java/com/example/healthtipstricks/ItemAdapter.java
+++ app/src/main/java/com/example/healthtipstricks/ItemAdapter.java
@@ -1,11 +1,23 @@
 package com.example.healthtipstricks;
 
+import android.content.Context;
 import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import java.util.List;
+
 public class ItemAdapter extends RecyclerView.Adapter<ItemViewHolder> {
+
+    Context context;
+    List<Data> dataList;
+
+    public ItemAdapter(Context context, List<Data> dataList) {
+        this.context = context;
+        this.dataList = dataList;
+    }
+
     @NonNull
     @Override
     public ItemViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
