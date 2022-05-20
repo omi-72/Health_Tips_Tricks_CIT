2b20437e2d344fe89fab87bb42d1513ab752ec02
omi-72 <jannat.omi722@gmail.com>
2022-05-20 23:48:05 +0600

    extend adapter <ItemViewHolder>

--- app/src/main/java/com/example/healthtipstricks/ItemAdapter.java
+++ app/src/main/java/com/example/healthtipstricks/ItemAdapter.java
@@ -1,4 +1,24 @@
 package com.example.healthtipstricks;
 
-public class ItemAdapter {
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class ItemAdapter extends RecyclerView.Adapter<ItemViewHolder> {
+    @NonNull
+    @Override
+    public ItemViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ItemViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
 }
