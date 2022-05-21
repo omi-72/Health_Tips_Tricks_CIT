dcc085d64c7435c803c497e4e90ff555906c040d
omi-72 <jannat.omi722@gmail.com>
2022-05-21 22:54:33 +0600

    backarrow set up

--- app/src/main/java/com/example/healthtipstricks/DetailsActivity2.java
+++ app/src/main/java/com/example/healthtipstricks/DetailsActivity2.java
@@ -3,12 +3,21 @@ package com.example.healthtipstricks;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class DetailsActivity2 extends AppCompatActivity {
+
+    ImageView img;
+    TextView title, desc;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_details2);
+
+        img= findViewById(R.id.img);
+        title= findViewById(R.id.title);
+        title= findViewById(R.id.desc);
     }
 }
